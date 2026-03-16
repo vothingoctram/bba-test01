9a08a8f0d790eb5a75a525dfab699536a77c8155
test091-tramvo <ngoctram300996@gmail.com>
2026-03-17 06:22:40 +0700

    feat: cloud-kingdom-2

---
+++ tests/lesson-04/cloud-kingdom.js
@@ -0,0 +1,17 @@
+let powderUP = "mushroom";
+if (mushroom = "mushroom"){
+    effect = " Mario becomes Supper!";
+}
+else if (powderUP = "flower"){
+    effect = " Mario can shoot fireballs !";
+}
+else if (powderUP = "star"){
+    effect = " Mario is invincible !";
+}
+else if (powderUP = "none"){
+    effect = " Mario is normal !";
+}
+else if (powderUP = "khac"){
+    effect = " UNKNOWN POWER-UP";
+}
+console.log('Effect of Mario : '+  effect);
